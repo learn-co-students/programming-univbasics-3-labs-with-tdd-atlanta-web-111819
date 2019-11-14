fc6d7357b9d8c9a6fc98c224005a347c9333d676
Treven Womack <keytonwomack@yahoo.com>
2019-11-14 17:29:43 +0000

    Done.

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,13 +2,13 @@
 
 describe "./calculator.rb" do
 
-  it "contains a local variable called first_number that is assigned to a number" do
-    first_number = get_variable_from_file('./calculator.rb', "first_number")
+  it "contains a local variable called first_number that is assigned to a number"
+  do
+   
+first_number = get_variable_from_file('./calculator.rb', "first_number")
+  it "contains a local variable called second_number that is assigned to a number that isn't 0"
 
-    expect(first_number).to be_an(Integer).or be_a(Float)
-  end
-
-  it "contains a local variable called second_number that is assigned to a number that isn't 0" do
+    expect(first_number).to be_an(Integer).or be_a(Float) end
     second_number = get_variable_from_file('./calculator.rb', "second_number")
 
     expect(second_number).to be_an(Integer).or be_a(Float)
