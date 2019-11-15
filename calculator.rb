72a324df202295704946714f0c2896e65f45c82b
Jessica Michael <pantherjmm@yahoo.com>
2019-11-15 01:15:24 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,19 @@
-# Add your variables here
+'''Ruby
+
+first_variable = 3
+second_variable = 4.5
+
+def #{first_variable}
+
+def #{second_variable}
+
+puts sum(#{first_variable} + {second_variable})
+
+puts difference(#{first_variable} - #{second_variable})
+
+puts product(#{first_variable} * #{second_variable})
+
+puts quotient(#{first_variable} / #{second_variable})
+
+end
+end
